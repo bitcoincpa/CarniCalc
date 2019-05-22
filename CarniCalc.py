2c03646877ac353378e72e19211c2c9507bcdb93
Dave Mag <davemagarian@Daves-MacBook-Pro-2.local>
2019-05-22 13:48:20 -0700

    Updated

--- CarniCalc.py
+++ CarniCalc.py
@@ -1,18 +1,22 @@
+import PriceList as pl
+
 print("Hello Carnis")
+#budget_input = "What is your daily budget?"
+#lb_input = "How many pounds of meat do you want to eat per day?"
 
 class Meal:
     def __init__(self, budget, lb):
         self.budget = budget
         self.lb = lb
+        self.price = pl.Prices()
         
 
 
-    def priceperlb(self, cost):
-        self.cost = cost
-        total= float(cost) * float(self.lb)
+    def priceperlb(self):
+        total= self.price.ribeye * self.lb
         return total
 
     
-ribeye = Meal(30,1)
+ribeye = Meal(10,10)
 
-print(ribeye.priceperlb(20))
+print(ribeye.priceperlb())
