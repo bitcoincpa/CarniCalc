7cff655758af9f93587c3480a92a8b2cf33298fc
Dave Mag <davemagarian@Daves-MacBook-Pro-2.local>
2019-05-22 13:49:04 -0700

    updated price list

--- PriceList.py
+++ PriceList.py
@@ -1,8 +1,8 @@
-class Price:
+class Prices:
     """A class to store all prices"""
 
     def __init__(self):
         #Initialize prices per lb
 
-        self.ribeye  = 11
+        self.ribeye  = 12
         self.ground_beef = 4
