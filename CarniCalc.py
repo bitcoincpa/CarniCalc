189aea584545969b156e25d700bc42f5385c7685
Dave Mag <davemagarian@Daves-MacBook-Pro-2.local>
2019-05-22 13:14:11 -0700

    testing a few lines

--- CarniCalc.py
+++ CarniCalc.py
@@ -1,1 +1,18 @@
 print("Hello Carnis")
+
+class Meal:
+    def __init__(self, budget, lb):
+        self.budget = budget
+        self.lb = lb
+        
+
+
+    def priceperlb(self, cost):
+        self.cost = cost
+        total= float(cost) * float(self.lb)
+        return total
+
+    
+ribeye = Meal(30,1)
+
+print(ribeye.priceperlb(20))
